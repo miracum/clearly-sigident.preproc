c619087eba1ddc7c32e8699396d402c3061225cc
Lorenz Kapsner <lorenz.kapsner@uk-erlangen.de>
2019-12-05 09:27:49 +0100

    fixed typo in vignette; incremented to v0.0.1

--- data-raw/devstuffs.R
+++ data-raw/devstuffs.R
@@ -19,7 +19,7 @@ my_desc$del("Maintainer")
 # Vignette Builder
 my_desc$set("VignetteBuilder" = "knitr")
 # Set the version
-my_desc$set_version("0.0.0.9000")
+my_desc$set_version("0.0.1")
 # The title of your package
 my_desc$set(Title = "Sigident Preprocessing")
 # The description of your package
