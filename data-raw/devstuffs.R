394a54fd4a0e267186e4897ab1cf393d20e11639
Lorenz Kapsner <lorenz.kapsner@uk-erlangen.de>
2019-12-05 09:28:51 +0100

    opened new development branch

--- data-raw/devstuffs.R
+++ data-raw/devstuffs.R
@@ -19,7 +19,7 @@ my_desc$del("Maintainer")
 # Vignette Builder
 my_desc$set("VignetteBuilder" = "knitr")
 # Set the version
-my_desc$set_version("0.0.1")
+my_desc$set_version("0.0.1.9000")
 # The title of your package
 my_desc$set(Title = "Sigident Preprocessing")
 # The description of your package
